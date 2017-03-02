5eab9c4d1f38e7b65011fc33355537e431f705b4
José Alberto <ovallesoft@gmail.com>
2017-03-01 21:50:53 -0600

    Ejemplo
    Ejemplo

---
+++ OvallesEjemplo/Controllers/SaludoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OvallesEjemplo.Controllers
+{
+    public class SaludoController : Controller
+    {
+        // GET: Saludo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
